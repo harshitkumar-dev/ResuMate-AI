7a5fec223fda292a947414c293ddb86c472eb1ba
Harshit Kumar <harshitkumar.dev1@gmail.com>
2026-07-05 12:50:55 +0530

    Initial commit

---
+++ client/src/components/home/Banner.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Banner = () => {
+  return ( 
+    <div className="w-full py-2.5 font-medium text-sm text-pink-800 text-center bg-gradient-to-r from-[#ff7ecb] to-[#FDFEFF]">
+        <p><span className="px-3 py-1 rounded-lg text-white bg-pink-600 mr-2">New</span>AI-Powered keyword targeting</p>
+    </div>
+  )
+}
+
+export default Banner
